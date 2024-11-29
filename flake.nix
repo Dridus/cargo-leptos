c8b1d6582afaf24144bfe3860757692ad5447c8e
Ross MacLeod <rmm+github@z.odi.ac>
2024-11-28 21:59:45 -0500

    bump flake.nix inputs

--- flake.nix
+++ flake.nix
@@ -1,20 +1,24 @@
-
 {
   description = "A basic Rust devshell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       with pkgs;
       {
@@ -28,7 +32,10 @@
             zlib.out
             llvmPackages_latest.lld
             (rust-bin.stable.latest.default.override {
-              extensions= [ "rust-src" "rust-analyzer" ];
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+              ];
               targets = [ "wasm32-unknown-unknown" ];
             })
           ];
@@ -37,7 +44,7 @@
             alias ls=exa
             alias find=fd
             alias grep=ripgrep
-            '';
+          '';
         };
       }
     );
